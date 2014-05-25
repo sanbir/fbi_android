3a8aa6c473947687f19899d1bf65abbf86026f4a
Aleksandr Biryukov <aleksbiryukov@gmail.com>
2014-05-25 15:00:52 +0700

    add transition

---
+++ App17/Selectors/MyItemTemlateSelector.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+using App17.Data;
+
+namespace App17.Selectors
+{
+    class MyItemTemlateSelector : DataTemplateSelector
+    {
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            SampleDataItem selectedItem = item as SampleDataItem;
+            String template;
+
+            if (selectedItem.ItemType == "browser") 
+            {
+                template = @"
+                <DataTemplate xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'>
+                    <WebView Source="""
+                    + selectedItem.ImagePath
+                    + @""" />
+                </DataTemplate>";
+            }
+            else if (selectedItem.ItemType == "photo")
+            {
+                template = @"
+                <DataTemplate xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'>
+                    <Image Source=""{Binding ImagePath}"" />
+                </DataTemplate>";
+            }
+            else if (selectedItem.ItemType == "user")
+            {
+                template = @"<DataTemplate xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation' xmlns:common='using:App17.Common'>
+                        <ScrollViewer Style=""{StaticResource HorizontalScrollViewerStyle}"" Grid.Row=""1"">
+                            <common:RichTextColumns Margin=""117,0,117,47"" VerticalAlignment=""Top"">
+                                <RichTextBlock Width=""560"" Style=""{StaticResource ItemRichTextStyle}"" IsTextSelectionEnabled=""False"">
+                                    <Paragraph>
+                                        <Run FontSize=""26.667"" FontWeight=""Light"" Text=""{Binding Title}""/>
+                                        <LineBreak/>
+                                        <LineBreak/>
+                                        <Run FontWeight=""Normal"" Text=""{Binding Subtitle}""/>
+                                    </Paragraph>
+                                    <Paragraph LineStackingStrategy=""MaxHeight"">
+                                        <InlineUIContainer>
+                                            <Image MaxHeight=""480"" Width=""300"" Margin=""0,20,0,10"" Stretch=""Uniform"" Source=""{Binding ImagePath}"" AutomationProperties.Name=""{Binding Title}""/>
+                                        </InlineUIContainer>
+                                    </Paragraph>
+                                    <Paragraph>
+                                        <Run FontWeight=""SemiLight"" Text=""{Binding Content}""/>
+                                    </Paragraph>
+                                </RichTextBlock>
+                                <common:RichTextColumns.ColumnTemplate>
+                                    <DataTemplate>
+                                        <RichTextBlockOverflow Width=""560"" Margin=""80,0,0,0"">
+                                            <RichTextBlockOverflow.RenderTransform>
+                                                <TranslateTransform X=""-1"" Y=""4""/>
+                                            </RichTextBlockOverflow.RenderTransform>
+                                        </RichTextBlockOverflow>
+                                    </DataTemplate>
+                                </common:RichTextColumns.ColumnTemplate>
+                            </common:RichTextColumns>
+                        </ScrollViewer>
+                    </DataTemplate>";
+            }
+            else if (selectedItem.ItemType == "launch")
+            {
+                template = @"
+                <DataTemplate xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'>
+                    <Button Content=""Открыть документ"" HorizontalAlignment=""Center"" VerticalAlignment=""Center"" FontSize=""40""  />
+                </DataTemplate>";
+            }
+            else if (selectedItem.ItemType == "table") 
+            {
+                if (selectedItem.Title == "Бакалавриат")
+                {
+                    template = @"<DataTemplate xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'>
+                    <ScrollViewer Style=""{StaticResource HorizontalScrollViewerStyle}"" Grid.Row=""1"">
+                        <Grid>
+                            <Grid.RowDefinitions>
+                                <RowDefinition Height=""50""/>
+                                <RowDefinition Height=""*""/>
+                            </Grid.RowDefinitions>
+                            <TextBlock Style=""{StaticResource PageSubheaderTextStyle}"" Margin=""70,0"">План набора на бакалавриат</TextBlock>
+                            <StackPanel Orientation=""Horizontal"" Margin=""70"" Grid.Row=""1"">
+                                <StackPanel>
+                                    <TextBlock Style=""{StaticResource GroupHeaderTextStyle}"" Margin=""10"">Направления и профиль</TextBlock>
+                                    <TextBlock Style=""{StaticResource TitleTextBlockStyle}"" Margin=""10"">Информационные системы и технологии</TextBlock>
+                                    <TextBlock Style=""{StaticResource TitleTextBlockStyle}"" Margin=""10"">Прикладная информатика</TextBlock>
+                                    <TextBlock Style=""{StaticResource TitleTextBlockStyle}"" Margin=""10"">Менеджмент Логистика и управление цепями поставок</TextBlock>
+                                </StackPanel>
+                                <StackPanel>
+                                    <TextBlock Style=""{StaticResource GroupHeaderTextStyle}"" Margin=""10"">Полное возмещение затрат</TextBlock>
+                                    <TextBlock Style=""{StaticResource TitleTextBlockStyle}"" Margin=""10"" TextAlignment=""Center"">30</TextBlock>
+                                    <TextBlock Style=""{StaticResource TitleTextBlockStyle}"" Margin=""10"" TextAlignment=""Center"">35</TextBlock>
+                                    <TextBlock Style=""{StaticResource TitleTextBlockStyle}"" Margin=""10"" TextAlignment=""Center"">75</TextBlock>
+                                </StackPanel>
+                                <StackPanel>
+                                    <TextBlock Style=""{StaticResource GroupHeaderTextStyle}"" Margin=""10"">Бюджетное финансирование</TextBlock>
+                                    <TextBlock Style=""{StaticResource TitleTextBlockStyle}"" Margin=""10"" TextAlignment=""Center"">20</TextBlock>
+                                    <TextBlock Style=""{StaticResource TitleTextBlockStyle}"" Margin=""10"" TextAlignment=""Center"">15</TextBlock>
+                                    <TextBlock Style=""{StaticResource TitleTextBlockStyle}"" Margin=""10"" TextAlignment=""Center"">8</TextBlock>
+                                </StackPanel>
+                                <StackPanel>
+                                    <TextBlock Style=""{StaticResource GroupHeaderTextStyle}"" Margin=""10"">Всего</TextBlock>
+                                    <TextBlock Style=""{StaticResource TitleTextBlockStyle}"" Margin=""10"" TextAlignment=""Center"">50</TextBlock>
+                                    <TextBlock Style=""{StaticResource TitleTextBlockStyle}"" Margin=""10"" TextAlignment=""Center"">50</TextBlock>
+                                    <TextBlock Style=""{StaticResource TitleTextBlockStyle}"" Margin=""10"" TextAlignment=""Center"">83</TextBlock>
+                                </StackPanel>
+                            </StackPanel>
+                        </Grid>
+                    </ScrollViewer>
+                </DataTemplate>";
+                }
+                else if (selectedItem.Title == "Магистратура")
+                {
+                    template = @"<DataTemplate xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'>
+                    <ScrollViewer Style=""{StaticResource HorizontalScrollViewerStyle}"" Grid.Row=""1"">
+                        <Grid>
+                            <Grid.RowDefinitions>
+                                <RowDefinition Height=""50""/>
+                                <RowDefinition Height=""*""/>
+                            </Grid.RowDefinitions>
+                            <TextBlock Style=""{StaticResource PageSubheaderTextStyle}"" Margin=""70,0"">План набора в магистратуру</TextBlock>
+                            <StackPanel Orientation=""Horizontal"" Margin=""70,50"" Grid.Row=""1"">
+                                <StackPanel>
+                                    <TextBlock Style=""{StaticResource GroupHeaderTextStyle}"" Margin=""10"">Направления и профиль</TextBlock>
+                                    <TextBlock Style=""{StaticResource TitleTextBlockStyle}"" Margin=""10"">Информационные системы и технологии</TextBlock>
+                                    <TextBlock Style=""{StaticResource TitleTextBlockStyle}"" Margin=""10"">Менеджмент Логистика и управление цепями поставок</TextBlock>
+                                </StackPanel>
+                                <StackPanel>
+                                    <TextBlock Style=""{StaticResource GroupHeaderTextStyle}"" Margin=""10"">Полное возмещение затрат</TextBlock>
+                                    <TextBlock Style=""{StaticResource TitleTextBlockStyle}"" Margin=""10"" TextAlignment=""Center"">5</TextBlock>
+                                    <TextBlock Style=""{StaticResource TitleTextBlockStyle}"" Margin=""10"" TextAlignment=""Center"">8</TextBlock>
+                                </StackPanel>
+                                <StackPanel>
+                                    <TextBlock Style=""{StaticResource GroupHeaderTextStyle}"" Margin=""10"">Бюджетное финансирование</TextBlock>
+                                    <TextBlock Style=""{StaticResource TitleTextBlockStyle}"" Margin=""10"" TextAlignment=""Center"">5</TextBlock>
+                                    <TextBlock Style=""{StaticResource TitleTextBlockStyle}"" Margin=""10"" TextAlignment=""Center"">2</TextBlock>
+                                </StackPanel>
+                                <StackPanel>
+                                    <TextBlock Style=""{StaticResource GroupHeaderTextStyle}"" Margin=""10"">Всего</TextBlock>
+                                    <TextBlock Style=""{StaticResource TitleTextBlockStyle}"" Margin=""10"" TextAlignment=""Center"">10</TextBlock>
+                                    <TextBlock Style=""{StaticResource TitleTextBlockStyle}"" Margin=""10"" TextAlignment=""Center"">10</TextBlock>
+                                </StackPanel>
+                            </StackPanel>
+                        </Grid>
+                    </ScrollViewer>
+                </DataTemplate>";
+                }
+                else
+                {
+                    template = @"<DataTemplate xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'>
+                    <TextBox Text=""Элемент отображается неправильно"" FontSize=""50"" />
+                </DataTemplate>";
+                }
+            }
+            else
+            {
+                template = @"<DataTemplate xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'>
+                    <TextBox Text=""Элемент отображается неправильно"" FontSize=""50"" />
+                </DataTemplate>";
+            }
+
+            return XamlReader.Load(template) as DataTemplate;
+        }
+    }
+}
---
+++ App17/Selectors/MyTheme.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media;
+
+namespace App17.Selectors
+{
+    public class MyTheme
+    {
+        public Brush LogoBrush { get; set; }
+        public Brush BackBrush { get; set; }
+        public Brush TileBackBrush { get; set; }
+        public Brush TileFrontBrush { get; set; }
+        public Brush PopupBrush { get; set; }
+
+
+        private static List<MyTheme> myThemeStaticList = new List<MyTheme>() 
+                {
+                    new MyTheme()
+                    {
+                        LogoBrush = XamlReader.Load(
+                            @"<LinearGradientBrush 
+                                    xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+                                    EndPoint=""0.5,1"" StartPoint=""0.5,0"">
+                                <GradientStop Color=""#FF1857DC"" Offset=""0""/>
+                                <GradientStop Color=""#FFC0CFF0"" Offset=""0.993""/>
+                            </LinearGradientBrush>"
+                        ) as LinearGradientBrush,
+                        BackBrush = XamlReader.Load(
+                            @"<LinearGradientBrush 
+                                    xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+                                    EndPoint=""0.5,1"" StartPoint=""0.5,0"">
+                                <GradientStop Color=""#FFE4EAF7""/>
+                                <GradientStop Color=""#FFECF0F9"" Offset=""0.142""/>
+                                <GradientStop Color=""#FFD0DAF5"" Offset=""0.582""/>
+                                <GradientStop Color=""#FF95AFE8"" Offset=""0.993""/>
+                            </LinearGradientBrush>"
+                        ) as LinearGradientBrush,
+                        TileBackBrush = new SolidColorBrush(Color.FromArgb(255,7,7,66)),
+                        TileFrontBrush = new SolidColorBrush(Colors.Red),
+                        PopupBrush = new SolidColorBrush(Colors.Plum)
+                    },
+                    new MyTheme()
+                    {
+                        LogoBrush = XamlReader.Load(
+                            @"<LinearGradientBrush 
+                                    xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+                                    EndPoint=""0.5,1"" StartPoint=""0.5,0"">
+                                <GradientStop Color=""#FFFF1800"" Offset=""0""/>
+                                <GradientStop Color=""#FFFF8C69"" Offset=""0.993""/>
+                            </LinearGradientBrush>"
+                        ) as LinearGradientBrush,
+                        BackBrush = XamlReader.Load(
+                            @"<LinearGradientBrush 
+                                    xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+                                    EndPoint=""0.5,1"" StartPoint=""0.5,0"">
+                                <GradientStop Color=""#FFFFE5E3"" Offset=""0""/>
+                                <GradientStop Color=""#FFF9A58A"" Offset=""0.993""/>
+                            </LinearGradientBrush>"
+                        ) as LinearGradientBrush,
+                        TileBackBrush = new SolidColorBrush(Color.FromArgb(255,71,3,0)),
+                        TileFrontBrush = new SolidColorBrush(Colors.WhiteSmoke),
+                        PopupBrush = new SolidColorBrush(Colors.Wheat)
+                    },
+                    new MyTheme()
+                    {
+                        LogoBrush = XamlReader.Load(
+                            @"<LinearGradientBrush 
+                                    xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+                                    EndPoint=""0.5,1"" StartPoint=""0.5,0"">
+                                <GradientStop Color=""#FF019E06"" Offset=""0""/>
+                                <GradientStop Color=""#FFA0F991"" Offset=""0.993""/>
+                            </LinearGradientBrush>"
+                        ) as LinearGradientBrush,
+                        BackBrush = XamlReader.Load(
+                            @"<LinearGradientBrush 
+                                    xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+                                    EndPoint=""0.5,1"" StartPoint=""0.5,0"">
+                                <GradientStop Color=""#FFE7F5E7"" Offset=""0""/>
+                                <GradientStop Color=""#FF99F789"" Offset=""0.993""/>
+                            </LinearGradientBrush>"
+                        ) as LinearGradientBrush,
+                        TileBackBrush = new SolidColorBrush(Color.FromArgb(255,10,58,2)),
+                        TileFrontBrush = new SolidColorBrush(Colors.Olive),
+                        PopupBrush = new SolidColorBrush(Colors.PowderBlue)
+                    },
+                    new MyTheme()
+                    {
+                        LogoBrush = XamlReader.Load(
+                            @"<LinearGradientBrush 
+                                    xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+                                    EndPoint=""0.5,1"" StartPoint=""0.5,0"">
+                                <GradientStop Color=""#FFF55900"" Offset=""0""/>
+                                <GradientStop Color=""#FFF7C9A3"" Offset=""0.993""/>
+                            </LinearGradientBrush>"
+                        ) as LinearGradientBrush,
+                        BackBrush = XamlReader.Load(
+                            @"<LinearGradientBrush 
+                                    xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+                                    EndPoint=""0.5,1"" StartPoint=""0.5,0"">
+                                <GradientStop Color=""#FFF9F1EC"" Offset=""0""/>
+                                <GradientStop Color=""#FFFBBA85"" Offset=""0.993""/>
+                            </LinearGradientBrush>"
+                        ) as LinearGradientBrush,
+                        TileBackBrush = new SolidColorBrush(Color.FromArgb(255,50,21,0)),
+                        TileFrontBrush = new SolidColorBrush(Colors.Red),
+                        PopupBrush = new SolidColorBrush(Colors.Plum)
+                    },
+                    new MyTheme()
+                    {
+                        LogoBrush = XamlReader.Load(
+                            @"<LinearGradientBrush 
+                                    xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+                                    EndPoint=""0.5,1"" StartPoint=""0.5,0"">
+                                <GradientStop Color=""#FF08B6B6"" Offset=""0""/>
+                                <GradientStop Color=""#FFAAF8F9"" Offset=""0.987""/>
+                            </LinearGradientBrush>"
+                        ) as LinearGradientBrush,
+                        BackBrush = XamlReader.Load(
+                            @"<LinearGradientBrush 
+                                    xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+                                    EndPoint=""0.5,1"" StartPoint=""0.5,0"">
+                                <GradientStop Color=""#FFF7FDFD"" Offset=""0""/>
+        <GradientStop Color=""#FFC4F3F3"" Offset=""0.987""/>
+                            </LinearGradientBrush>"
+                        ) as LinearGradientBrush,
+                        TileBackBrush = new SolidColorBrush(Color.FromArgb(255,3,104,104)),
+                        TileFrontBrush = new SolidColorBrush(Colors.Red),
+                        PopupBrush = new SolidColorBrush(Colors.Plum)
+                    },
+                    new MyTheme()
+                    {
+                        LogoBrush = XamlReader.Load(
+                            @"<LinearGradientBrush 
+                                    xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+                                    EndPoint=""0.5,1"" StartPoint=""0.5,0"">
+                                <GradientStop Color=""#FF9711B6"" Offset=""0""/>
+                                <GradientStop Color=""#FFEE96E6"" Offset=""0.993""/>
+                            </LinearGradientBrush>"
+                        ) as LinearGradientBrush,
+                        BackBrush = XamlReader.Load(
+                            @"<LinearGradientBrush 
+                                    xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+                                    EndPoint=""0.5,1"" StartPoint=""0.5,0"">
+                                <GradientStop Color=""#FFF8ECFB"" Offset=""0""/>
+                                <GradientStop Color=""#FFE8AEE3"" Offset=""0.993""/>
+                            </LinearGradientBrush>"
+                        ) as LinearGradientBrush,
+                        TileBackBrush = new SolidColorBrush(Color.FromArgb(255,68,1,62)),
+                        TileFrontBrush = new SolidColorBrush(Colors.Red),
+                        PopupBrush = new SolidColorBrush(Colors.Plum)
+                    }
+                };
+
+        public static MyThemesNames CurrentThemeName { get; set; }
+
+        public static MyTheme CurrentTheme 
+        {
+            get
+            {
+                return myThemeStaticList[(int)CurrentThemeName];
+            }
+        }
+    }
+
+    public enum MyThemesNames 
+    {
+        Blue,
+        Red,
+        Green,
+        Orange,
+        Cyan,
+        Violet
+    }
+}

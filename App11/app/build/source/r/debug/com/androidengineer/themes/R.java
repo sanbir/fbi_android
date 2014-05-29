9c6f331a46f203fe44a72e52e108af2c682b142a
Aleksandr Biryukov <aleksbiryukov@gmail.com>
2014-05-29 14:56:51 +0700

    edit user

--- App11/app/build/source/r/debug/com/androidengineer/themes/R.java
+++ App11/app/build/source/r/debug/com/androidengineer/themes/R.java
@@ -154,10 +154,11 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int white_container_wh=0x7f020023;
     }
     public static final class id {
-        public static final int image=0x7f080002;
-        public static final int text=0x7f080003;
-        public static final int text1=0x7f080000;
-        public static final int text2=0x7f080001;
+        public static final int image=0x7f080003;
+        public static final int scroll=0x7f080000;
+        public static final int text=0x7f080004;
+        public static final int text1=0x7f080001;
+        public static final int text2=0x7f080002;
     }
     public static final class layout {
         public static final int main=0x7f030000;

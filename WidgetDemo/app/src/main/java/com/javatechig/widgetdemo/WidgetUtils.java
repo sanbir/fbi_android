f8983c151cd7432afda05dac187561dcabe80b11
Aleksandr Biryukov <aleksbiryukov@gmail.com>
2014-06-01 23:26:13 +0700

    normal file saving

---
+++ WidgetDemo/app/src/main/java/com/javatechig/widgetdemo/WidgetUtils.java
@@ -0,0 +1,5 @@
+package com.javatechig.widgetdemo;
+
+public class WidgetUtils {
+	final static String WIDGET_UPDATE_ACTION ="com.javatechig.intent.action.UPDATE_WIDGET";
+}
--- app3/app/src/main/java/com/manish/customgridview/StudyActivity.java
+++ app3/app/src/main/java/com/manish/customgridview/StudyActivity.java
@@ -7,13 +7,25 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 
 public class StudyActivity extends Activity
@@ -70,7 +82,70 @@ public class StudyActivity extends Activity
                         }
                         break;
                     case 1:
-                        //startActivity (new Intent(getApplicationContext(), StartActivity.class));
+                        InputStream in = null;
+                        try {
+                            in = getApplicationContext().getAssets().open("uch_plan_mml.xls");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        File file1 = new File(getApplicationContext().getExternalFilesDir(
+                                Environment.DIRECTORY_DOCUMENTS), "fssdf54");
+
+                        //File file1 = new File(getApplicationContext().getFilesDir(), "fdsfs");
+                        //File file1 = getApplicationContext().getDir("eee", MODE_PRIVATE);
+                        OutputStream out = null;
+                        try {
+                            out = new FileOutputStream(file1);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            copyCompletely(in, out);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            out.flush();
+                            out.close();
+                            in.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        Uri path1 = Uri.fromFile(file1);
+
+                        // Parse the file into a uri to share with another application
+
+                        Intent newIntent1 = new Intent(Intent.ACTION_VIEW);
+                        newIntent1.setDataAndType(path1, "application/vnd.ms-excel");
+                        newIntent1.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        newIntent1.addFlags(
+                                Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+
+                        try {
+                            startActivity(newIntent1);
+                        } catch (ActivityNotFoundException ex) {
+                            ex.printStackTrace();
+                        }
+
+
+                       /* BufferedReader reader = null;
+                        try {
+                            reader = new BufferedReader(new InputStreamReader(raw, "UTF8"));
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }*/
+
+
+
+                        /*try {
+                            Toast.makeText(StudyActivity.this, reader.readLine() + reader.readLine() + reader.readLine(),
+                                    Toast.LENGTH_SHORT).show();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }*/
                         break;
                     case 2:
                         //startActivity (new Intent(getApplicationContext(), StartActivity.class));
@@ -82,6 +157,52 @@ public class StudyActivity extends Activity
 
             }
         });
+    }
+
+    public static void copyStream(InputStream input, OutputStream output)
+            throws IOException
+    {
+        byte[] buffer = new byte[1024]; // Adjust if you want
+        int bytesRead;
+        while ((bytesRead = input.read(buffer)) != -1)
+        {
+            output.write(buffer, 0, bytesRead);
+        }
+    }
+
+    public static void copyCompletely(InputStream input, OutputStream output) throws IOException {
+        // if both are file streams, use channel IO
+        if ((output instanceof FileOutputStream) && (input instanceof FileInputStream)) {
+            try {
+                FileChannel target = ((FileOutputStream) output).getChannel();
+                FileChannel source = ((FileInputStream) input).getChannel();
+
+                source.transferTo(0, Integer.MAX_VALUE, target);
+
+                source.close();
+                target.close();
+
+                return;
+            } catch (Exception e) { /* failover to byte stream version */
+            }
+        }
+
+        byte[] buf = new byte[8192];
+        while (true) {
+            int length = input.read(buf);
+            if (length < 0)
+                break;
+            output.write(buf, 0, length);
+        }
+
+        try {
+            input.close();
+        } catch (IOException ignore) {
+        }
+        try {
+            output.close();
+        } catch (IOException ignore) {
+        }
     }
 
 }

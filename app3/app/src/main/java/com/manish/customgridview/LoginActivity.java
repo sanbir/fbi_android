2b172006b6d587dc6230cc7cf880c057e91f3a7d
Aleksandr Biryukov <aleksbiryukov@gmail.com>
2014-06-03 16:17:31 +0700

    psw exp

--- app3/app/src/main/java/com/manish/customgridview/LoginActivity.java
+++ app3/app/src/main/java/com/manish/customgridview/LoginActivity.java
@@ -3,11 +3,23 @@ package com.manish.customgridview;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.*;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity
 {
+    PopupWindow popUp;
+    LinearLayout layout;
+    TextView tv;
+    LayoutParams params;
+    LinearLayout mainLayout;
+    Button but;
+    boolean click = true;
 
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -26,10 +38,61 @@ public class LoginActivity extends Activity
                     value = extras.getString("accountType");
                 }
 
+                TextView txtUserName = (TextView) findViewById(R.id.txtUserName);
+                TextView txtPassword = (TextView) findViewById(R.id.txtPassword);
+
                 if (value.equals("student")) {
-                    startActivity (new Intent(getApplicationContext(), StudentActivity.class));
+                    /*f (txtUserName.getText()==("qwe")
+                            && txtPassword.getText()==("123")) {*/
+                        startActivity (new Intent(getApplicationContext(), StudentActivity.class));
+                    /*} else {
+                        *//*popUp = new PopupWindow(LoginActivity.this);
+                        layout = new LinearLayout(LoginActivity.this);
+                        mainLayout = new LinearLayout(LoginActivity.this);
+                        tv = new TextView(LoginActivity.this);
+                        but = new Button(LoginActivity.this);
+                        but.setText("Click Me");
+                        but.setOnClickListener(new View.OnClickListener() {
+
+                            public void onClick(View v) {
+                                if (click) {
+                                    popUp.showAtLocation(mainLayout, Gravity.BOTTOM, 10, 10);
+                                    popUp.update(50, 50, 300, 80);
+                                    click = false;
+                                } else {
+                                    popUp.dismiss();
+                                    click = true;
+                                }
+                            }
+
+                        });
+                        params = new LayoutParams(LayoutParams.WRAP_CONTENT,
+                                LayoutParams.WRAP_CONTENT);
+                        layout.setOrientation(LinearLayout.VERTICAL);
+                        tv.setText("Hi this is a sample text for popup window");
+                        layout.addView(tv, params);
+                        popUp.setContentView(layout);
+                        // popUp.showAtLocation(layout, Gravity.BOTTOM, 10, 10);
+                        mainLayout.addView(but, params);
+                        setContentView(mainLayout);*//*
+
+                        Toast.makeText(LoginActivity.this, "Неверная пара логин-пароль",
+						Toast.LENGTH_LONG).show();
+
+                    }*/
+
+
                 } else if (value.equals("teacher")) {
-                    startActivity (new Intent(getApplicationContext(), TeacherActivity.class));
+/*                    if (txtUserName.getText().equals("qwe")
+                            && txtPassword.getText().equals("123")) {*/
+                        startActivity (new Intent(getApplicationContext(), TeacherActivity.class));
+/*                    } else {
+
+                        Toast.makeText(LoginActivity.this, "Неверная пара логин-пароль",
+                                Toast.LENGTH_LONG).show();
+
+                    }*/
+
                 }
             }
         });

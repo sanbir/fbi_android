094ad9905d636bfb0e6beef51d3203ba0db3b541
Aleksandr Biryukov <aleksbiryukov@gmail.com>
2014-06-07 05:08:08 +0700

    add login

--- app3/app/src/main/java/com/manish/customgridview/LoginActivity.java
+++ app3/app/src/main/java/com/manish/customgridview/LoginActivity.java
@@ -30,7 +30,6 @@ public class LoginActivity extends Activity
         okBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // add password check!!!
 
                 String value = "";
                 Bundle extras = getIntent().getExtras();
@@ -42,64 +41,30 @@ public class LoginActivity extends Activity
                 TextView txtPassword = (TextView) findViewById(R.id.txtPassword);
 
                 if (value.equals("student")) {
-                    /*f (txtUserName.getText()==("qwe")
-                            && txtPassword.getText()==("123")) {*/
 
-                    Intent i = new Intent(getApplicationContext(), StudentActivity.class);
-                    i.putExtra("accountType","teacher");
-                    //i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(i);
-                    finish();
-                    /*} else {
-                        *//*popUp = new PopupWindow(LoginActivity.this);
-                        layout = new LinearLayout(LoginActivity.this);
-                        mainLayout = new LinearLayout(LoginActivity.this);
-                        tv = new TextView(LoginActivity.this);
-                        but = new Button(LoginActivity.this);
-                        but.setText("Click Me");
-                        but.setOnClickListener(new View.OnClickListener() {
+                    if (txtUserName.getText().toString().equals("stu")
+                            && txtPassword.getText().toString().equals("123")) {
 
-                            public void onClick(View v) {
-                                if (click) {
-                                    popUp.showAtLocation(mainLayout, Gravity.BOTTOM, 10, 10);
-                                    popUp.update(50, 50, 300, 80);
-                                    click = false;
-                                } else {
-                                    popUp.dismiss();
-                                    click = true;
-                                }
-                            }
-
-                        });
-                        params = new LayoutParams(LayoutParams.WRAP_CONTENT,
-                                LayoutParams.WRAP_CONTENT);
-                        layout.setOrientation(LinearLayout.VERTICAL);
-                        tv.setText("Hi this is a sample text for popup window");
-                        layout.addView(tv, params);
-                        popUp.setContentView(layout);
-                        // popUp.showAtLocation(layout, Gravity.BOTTOM, 10, 10);
-                        mainLayout.addView(but, params);
-                        setContentView(mainLayout);*//*
-
+                        startActivity(new Intent(getApplicationContext(), StudentActivity.class));
+                        finish();
+                    } else {
                         Toast.makeText(LoginActivity.this, "Неверная пара логин-пароль",
 						Toast.LENGTH_LONG).show();
-
-                    }*/
-
+                    }
 
                 } else if (value.equals("teacher")) {
-/*                    if (txtUserName.getText().equals("qwe")
-                            && txtPassword.getText().equals("123")) {*/
-                        startActivity (new Intent(getApplicationContext(), TeacherActivity.class));
-                    finish();
-/*                    } else {
 
+                    if (txtUserName.getText().toString().equals("stu")
+                            && txtPassword.getText().toString().equals("123")) {
+                        startActivity (new Intent(getApplicationContext(), TeacherActivity.class));
+                        finish();
+                    } else {
                         Toast.makeText(LoginActivity.this, "Неверная пара логин-пароль",
                                 Toast.LENGTH_LONG).show();
-
-                    }*/
+                    }
 
                 }
+
             }
         });
 
